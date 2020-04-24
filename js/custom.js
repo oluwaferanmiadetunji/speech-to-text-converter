8ac7fd0212b89ba531f03d693980acea1e115688
Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-04-24 08:33:33 +0100

    Final version April 24 2020

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+function display() {
+    document.location.href = 'record.html'
+}
+function displayContact() {
+    document.location.href = 'contact.html'
+}
+function displayOutput() {
+    document.location.href = 'output.html'
+}
